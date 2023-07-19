b61039664249746e76297f9f8d47d55b06ebfeb1
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2023-07-19 22:36:24 +0000

    Fix ERROR ReferenceError: localStorage is not defined

--- src/app/services/current-user.service.ts
+++ src/app/services/current-user.service.ts
@@ -1,17 +1,21 @@
-import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrentUserService {
 
-  currentUser = new BehaviorSubject<{ id: string; name: string } | null | undefined >(undefined);
+  currentUser = new BehaviorSubject<{ id: string; name: string } | null | undefined>(undefined);
+  platformId: Object;
 
-  constructor() { }
+  constructor(@Inject(PLATFORM_ID) platformId: Object) {
+    this.platformId = platformId;
+  }
 
   setCurrentUser() {
-    if (localStorage.getItem('token')) {
+    if (isPlatformBrowser(this.platformId) && localStorage.getItem('token')) {
       this.currentUser.next({ id: '1', name: 'Foo' });
     } else {
       this.currentUser.next(null);
