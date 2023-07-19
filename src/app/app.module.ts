df46db5d1a16ccf6f1c3601068698a451cc1cefb
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2023-07-19 23:06:01 +0000

    Update Home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CurrentUserService } from './services/current-user.service';
+import { UsersTableModule } from './users-table/users-table.module';
+import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    UsersTableModule,
   ],
   providers: [CurrentUserService],
   bootstrap: [AppComponent]
