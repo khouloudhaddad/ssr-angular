3d05318c2b3ca0742e3c2333e0b6d5a13537d91c
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2023-07-19 22:32:33 +0000

    Dependency Injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CurrentUserService } from './services/current-user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   title = 'angular-seo';
+
+  constructor(private currentUserService: CurrentUserService) { }
+
+  ngOnInit(): void {
+    this.currentUserService.setCurrentUser();
+  }
+
 }
